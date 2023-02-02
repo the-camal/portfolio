769a4674b10f77c1fcafef0816548ae88403830c
paulcourts <hubertcourts@gmail.com>
2023-02-02 14:53:45 -0600

    last push have to get turned on

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import "../styles/nav.css";
 
 function navbar() {
+
   return (
-    <div className="navbar">
+    <div classname="navbar">
         <div className="barButton">
-            
-            <button></button>
+      
+        <button type="button">home</button>
+
         </div>
         <div className="links">
-            <Link to="/home"> home </Link>
+            <Link to="/home"> Paul.c </Link>
+
             <Link to="/skills">tech-skills</Link>
+            
             <Link to="/repos">git-repos</Link>
+
         </div>
     </div>
   )
